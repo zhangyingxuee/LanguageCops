826cbf17f366fc3e6558847e51a64598029ba1b8
zhangyingxuee <e0775185@u.nus.edu>
2022-05-28 13:54:11 +0800

    Build the game
    Fixed scene change bug, added exit game button

--- My project/Assets/Scripts/PhoneButtons/PhoneButtonUI.cs
+++ My project/Assets/Scripts/PhoneButtons/PhoneButtonUI.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PhoneButtonUI : MonoBehaviour
 {
-    public Object NextScene;
+    //public Object NextScene;
+    public int SceneIndex;
     public void LoadNextScene()
     {
-        SceneManager.LoadScene(NextScene.name);
+        SceneManager.LoadScene(SceneIndex);
+       
     }
     
 }
