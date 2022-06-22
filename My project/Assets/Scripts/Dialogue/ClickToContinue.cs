ec50d424b15ca9afd406cfdd55e777e05648fa3c
zhangyingxuee <e0775185@u.nus.edu>
2022-06-22 10:41:12 +0800

    some updates

--- My project/Assets/Scripts/Dialogue/ClickToContinue.cs
+++ My project/Assets/Scripts/Dialogue/ClickToContinue.cs
@@ -9,9 +9,9 @@ public class ClickToContinue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
+        
            if (Input.GetMouseButtonDown(0) && choiceButton.activeSelf == false)
             {
-
                 FindObjectOfType<DialogueManagerInk>().ContinueStory();
             }
              
